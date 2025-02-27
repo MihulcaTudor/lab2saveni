fc1fae5cd2e8dbd54befa242512b9ccd13205044
aidascripca <aida-maria.scripca@student.tuiasi.ro>
2025-02-27 19:04:14 +0200

    a treia incercare + 7 pastile

--- Pippr/src/saveni/TestSaveni.java
+++ Pippr/src/saveni/TestSaveni.java
@@ -4,7 +4,8 @@ public class TestSaveni {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("a doua incercare....");
+		//System.out.println("a doua incercare....");
+		System.out.println("a treia incercare + 1 martie muncitoresc");
 
 	}
 
