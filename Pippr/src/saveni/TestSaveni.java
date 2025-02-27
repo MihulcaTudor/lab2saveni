5da3dba322a694006b7f96625e167243b81cfe32
aidascripca <aida-maria.scripca@student.tuiasi.ro>
2025-02-27 19:05:15 +0200

    commit ACD

--- Pippr/src/saveni/TestSaveni.java
+++ Pippr/src/saveni/TestSaveni.java
@@ -5,7 +5,8 @@ public class TestSaveni {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//System.out.println("a doua incercare....");
-		System.out.println("a treia incercare + 1 martie muncitoresc");
+		//System.out.println("a treia incercare + 1 martie muncitoresc");
+		System.out.println("costin catre balamuc + 1 mai + ceai ba + etc ");
 
 	}
 
